992205f89a742034ac369d9016989316e78e4932
Iván Casas <73560533+ivanool@users.noreply.github.com>
2023-10-15 12:02:50 +0000

    2.0

--- Guerreros.java
+++ Guerreros.java
@@ -1,52 +1,50 @@
-public class Guerreros extends Personajes{
-    //ATRIBUTOS
-    String nombre;
-    string arma;
-    int energia;
-    int energiaInicial;
-    int energiaConsumo;
-    int vida = 100;
+public class Guerreros extends Personajes {
+    // Atributos específicos de Guerreros
+    private String arma;
 
-    //Constructores
-    public Guerreros (String nombre, String arma, int energiaInicial, int energiaConsumo){
-        if (energiaConsumo <10 && energiaConsumo > 20){
-            System.out.printnl("nivel de consumo de energia en arma fuera del rango");
-        }
-        
-            }
-
-    //MÉTODOS
-    public void combatir(){
-        energia = energiaInicial - energiaConsumo;
-        System.out.println("Guerrero "+nombre+"uso el arma "+arma+"y su nuevo nivel de energia es "+energia);
+    // Constructor de Guerreros
+    public Guerreros(String nombre, String arma, int energia, int energia_gastada) {
+        super(nombre, energia, energia_gastada);
+        this.arma = arma;
+        this.vida = 100; // Inicializamos la vida en 100
+        this.energia_gastada = energia_gastada;
+        this.energia = energia;
     }
-    public void Alimentarse(){
+
+    // Método para que un Guerrero combata
+    public void combatir() {
+        int energiaGastada = this.energia_gastada;
+        int energiaAnterior = energia;
+        energia -= energiaGastada;
+        System.out.println("Guerrero " + nombre + " [arma: " + arma + ", energía gastada: " + energiaGastada +", nivel de energía anterior: " + energiaAnterior + ", energía actual: " + energia + "]");
+    }
+
+    // Método para alimentar a un Guerrero (agrega 30 de energía)
+    @Override
+    public void Alimentarse() {
+        int energiaAnterior = energia;
         super.Alimentarse();
-
-        System.put.println("Guerrero [energia anterior: "+energiaInicial+", energia actual "+energia+"]");
+        System.out.println("Guerrero [energía anterior: " + energiaAnterior + ", energía actual: " + energia + "]");
     }
-    public void ConteoVida(){
 
+    // Método para verificar la vida del Guerrero
+    public void ConteoVida() {
+        if (vida <= 0) {
+            System.out.println("El guerrero " + nombre + " ha muerto");
+        } else {
+            System.out.println("El guerrero " + nombre + " tiene " + vida + " puntos de vida");
+        }
     }
-    public void NumAleatorio (){
-        int NumeroAleatorio; //creacion de variable con comando numero aleatorio
-            if(NumeroAleatorio >=1 && NumeroAleatorio <=49){
-                System.out.println("No lastimó");
-                if else (NumeroAleatorio >=50 && NumeroAleatorio <=70){
-                    System.out.println("Guerrero perdió 10 puntos de vida");
-                    vida = vida - 10;
-                }
-                    if e =e(NumeroAl eatorio >=71 && NumeroAleatorio <=89){
-                    System.out.println("Guerrero perdió 20 puntos de vida");
-                    vida = vida - 20 ;
-                    }
-                    else {
-                    System.out.println("Guerrero perdió 30 puntos de vida");
-                    vida = vida - 30;
-                }
-            }
+
+    @Override
+
+    public void mostrar_info() {
+        super.mostrar_info();
+        System.out.println("Arma: " + this.arma);
+        System.out.println("Vida: " + this.vida);
+    }
 }
-}
+
 
 
 
--- Magos.java
+++ Magos.java
@@ -1,22 +1,29 @@
 public class Magos extends Personajes{
     // Atributos
     int UsoDeEnergía;
-    static final int EnergíaInicial = 100;
-    int Vida;
+    String poder;
 
     // Constructores
-    public Magos(String nombre, String poder){
-        super(nombre, poder);
-        energia = EnergíaInicial;
-        System.out.println("El mago " + nombre + " tiene un poder " + poder +
-                            "con una energía inicial de " + EnergíaInicial);
-        int vida = 100;
+    public Magos(String nombre, String poder, int energia, int energia_gastada){
+        super(nombre, energia, energia_gastada);
+        this.poder = poder;
+        this.vida = 100;
+        this.energia_gastada = energia_gastada;
+        this.energia = energia;
     }
 
     // Métodos 
-    public void Encantar(int UsoDeEnergía){
+
+    @Override
+    public void Alimentarse(){
+        int energiaAnterior = energia;
+        super.Alimentarse();
+        System.out.println("Mago [energía anterior: " + energiaAnterior + ", energía actual: " + energia + "]");
+    }
+    public void Encantar(){
+        int UsoDeEnergía = this.energia_gastada;
         System.out.print("Mago " + nombre + " [Poder: " + poder + ", Energía gastada: "
-                            + UsoDeEnergía + ",  Nivel de energía anterior: " + energia +
+                            + this.energia_gastada + ",  Nivel de energía anterior: " + energia +
                             ", Energía actual: ");
         energia = energia - UsoDeEnergía;
         System.out.println(energia);
@@ -26,46 +33,7 @@ public class Magos extends Personajes{
     public void mostrar_info(){
         super.mostrar_info();
         System.out.println("Poder: "+this.poder);
+        System.out.println("Vida: "+this.vida);
     }
-
-    public int modificar_vida(int valor_random){
-        if(valor_random >1 && valor_random <49){
-            System.out.println("El mago "+this.nombre+" no ha perdido vida");
-            int estado = verificar_vida();
-            return estado;
-        }
-        else if(valor_random >=50 && valor_random <=70){
-            vida = vida - 10;
-            System.out.printl("El mago "+this.nombre+" ha perdido 10 puntos de vida");
-            int estado = verificar_vida();
-            return estado;
-        }
-        else if(valor_random >=71 &&valor_random<=89){
-            vida = vida-20;
-            System.out.printl("El mago "+this.nombre+" ha perdido 20 puntos de vida");
-            int estado =verificar_vida();
-            return estado;
-
-        }
-        else if(valor_random >=90 && valor_random <=99){
-            vida = vida - 30;
-            System.out.println("El mago "+this.nombre+" ha perdido 30 puntos de vida");
-            int estado =verificar_vida();
-            return estado;
-        }
-        else if(valor_random == 100){
-            return 0;
-        }
-    }
-
-    public int verificar_vida(){
-        if(vida <= 0){
-            System.out.println("El mago "+this.nombre+" ha muerto");
-            return -1;
-        }
-        else{
-            System.out.println("El mago "+this.nombre+" tiene "+this.vida+" puntos de vida");
-            return 1;
-        }
-    }
+    
 }
