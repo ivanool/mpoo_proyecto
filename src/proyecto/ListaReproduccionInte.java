e59bde38aa92c10a092784384120c8bb54aeee9f
SotoEsau <112227479+SotoEsau@users.noreply.github.com>
2023-10-21 19:59:19 -0600

    ListaReproduccionInte.java
    ListaReproduccionInte modificada con cajas de dialogo añadidas

--- src/proyecto/ListaReproduccionInte.java
+++ src/proyecto/ListaReproduccionInte.java
@@ -1,9 +1,18 @@
 package proyecto;
-
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListaReproduccionInte extends DispositivosInteligentes {
+
+    ImageIcon reproducir =new ImageIcon("src/proyecto/reproducir.jpeg");
+    ImageIcon vacia =new ImageIcon("src/proyecto/vacia.png");
+    ImageIcon pausa =new ImageIcon("src/proyecto/pausa.png");
+    ImageIcon detenida =new ImageIcon("src/proyecto/detenida.jpeg");
+    ImageIcon actualizada =new ImageIcon("src/proyecto/actualizada.png");
+    ImageIcon agregada =new ImageIcon("src/proyecto/agregada.png");
+    
     private List<String> listaReproduccion;
     private boolean reproduciendo;
 
@@ -16,29 +25,30 @@ public class ListaReproduccionInte extends DispositivosInteligentes {
     public void Reproducir() {
         if (!listaReproduccion.isEmpty()) {
             reproduciendo = true;
-            System.out.println("Reproduciendo " + listaReproduccion.get(0));
+            JOptionPane.showMessageDialog(null, "Reproduciendo " +  listaReproduccion.get(0), "Jaime, Navarro, Perez y Soto", JOptionPane.DEFAULT_OPTION, reproducir);
         } else {
-            System.out.println("La lista de reproducción está vacía.");
+            JOptionPane.showMessageDialog(null, "La lista de reproducción está vacía.", "Jaime, Navarro, Perez y Soto", JOptionPane.DEFAULT_OPTION, vacia); 
         }
     }
 
     public void Pausar() {
         reproduciendo = false;
-        System.out.println("Reproducción en pausa");
+        JOptionPane.showMessageDialog(null, "Reproducción en pausa", "Jaime, Navarro, Perez y Soto", JOptionPane.DEFAULT_OPTION, pausa);
+        
     }
 
     public void Detener() {
         reproduciendo = false;
-        System.out.println("Reproducción detenida");
+        JOptionPane.showMessageDialog(null, "Reproducción detenida", "Jaime, Navarro, Perez y Soto", JOptionPane.DEFAULT_OPTION, detenida);
     }
 
     public void CambiarListaReproduccion(List<String> nuevaLista) {
         listaReproduccion = nuevaLista;
-        System.out.println("Lista de reproducción actualizada");
+        JOptionPane.showMessageDialog(null, "Lista de reproducción actualizada", "Jaime, Navarro, Perez y Soto", JOptionPane.DEFAULT_OPTION, actualizada););
     }
 
     public void AgregarCancionALista(String cancion) {
         listaReproduccion.add(cancion);
-        System.out.println("Canción " + cancion + " agregada a la lista de reproducción");
+        JOptionPane.showMessageDialog(null, "Canción " + cancion + " agregada a la lista de reproducción", "Jaime, Navarro, Perez y Soto", JOptionPane.DEFAULT_OPTION, agregada);
     }
 }
